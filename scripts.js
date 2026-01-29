5e20a3a1de44b51fccdf88783e078b489ced3857
tnine1 <byiringirofiston18@gmail.com>
2026-01-29 22:29:46 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- scripts.js
+++ scripts.js
@@ -867,4 +867,34 @@ if (chatInput) {
   });
 }
 
+const CACHE_NAME = "lechic-cache-v1";
 
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registered"))
+    .catch(err => console.log("SW error:", err));
+}
